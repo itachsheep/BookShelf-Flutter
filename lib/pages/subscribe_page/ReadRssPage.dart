bf96b0feb148e995449fd4a28cc6cd76774f6df9
itachsheep <itachsheep@163.com>
2020-07-21 08:53:20 +0800

    init RssReadPage WebView

---
+++ lib/pages/subscribe_page/ReadRssPage.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+
+class ReadRssPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    throw UnimplementedError();
+  }
+}
+
+class ReadRssPageState extends State<ReadRssPage> {
+  @override
+  Widget build(BuildContext context) {
+    throw UnimplementedError();
+  }
+
+}
